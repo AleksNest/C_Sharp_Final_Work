adf12ccc2c39ff9575ce2c9199084466e86fd17a
Alexx <al_nesteov@inbox.ru>
2022-05-10 09:51:53 +0300

    add code files

---
+++ CodeControl/Program.cs
@@ -0,0 +1,38 @@
+﻿// control work
+
+string [] arrayinput = {"Надеюсь", "что", "все", "решил", "правильно", "!"};
+int N = 3;
+Console.WriteLine();
+Console.Write("Исходный массив:  ");
+PrintArray (arrayinput);
+Console.Write("Новый массив из строк <=" + N +" символов: " );
+PrintArray (TrancationArray (arrayinput, N));
+Console.WriteLine();
+string [] TrancationArray (string [] array, int n)          // метод создания нового массива строк из исходного по заданному кол символов
+{
+   int count = 0;
+   for (int i = 0; i < array.Length; i++) 
+   {
+       if (array[i].Length <= n) count++;   
+   }
+   string [] arraynew = new string [count];
+   int j = 0;
+   for (int i = 0; i < array.Length; i++) 
+   {
+       if (array[i].Length <= n) 
+       {
+           arraynew [j] = array[i]; 
+           j++;
+       }
+   }
+   return arraynew;
+}
+void PrintArray (string [] array)                         // метод печати массива
+{
+    Console.Write ("[ ");
+    for (int i = 0; i < array.Length; i++) 
+    {
+    Console.Write ("'"+ array[i] +"'"+ " ");
+    }
+    Console.WriteLine ("]");
+}
